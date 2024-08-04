1402cf10301a1008c1ff194d3a6a81cc0828c1e4
Hansajaa <nilanahansaja21@gmail.com>
2024-08-05 02:22:20 +0530

    Bug Fix

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -11,7 +11,6 @@ function LoginPage() {
   const {register, handleSubmit, formState:{errors}} = useForm();
 
   const onSubmit = (data) => {
-      console.log(data);
       authenticateUser(data);
   }
 
